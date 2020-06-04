1c463a38207d9f03220c9edad15e9ed25530cff1
francoing <franco.montti.19@gmail.com>
2020-06-04 19:53:00 -0300

    Nombre la tentacion

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -49,7 +49,7 @@
                 
 
                     <a class="navbar-brand" href="{{ url('/') }}">
-                    ALmacen
+                    La Tentacion
                     </a>
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
